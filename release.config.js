1d86db2db5f35b7bdce2cd13afc36d3e29e349d3
Andrea Aramini, INI-MBM-BNC (EXT) <andrea.aramini@swisscom.com>
2024-03-16 18:43:40 +0100

    :rocket: fix semver

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
-import { resolve } from 'path';
-import { readFileSync } from 'fs';
+const path = require('path');
+const fs = require('fs');
 
-const tplFile = resolve(__dirname, 'build/release-notes.hbs');
+const tplFile = path.resolve(__dirname, 'build/release-notes.hbs');
 
 export const branches = ['main'];
 export const plugins = [
@@ -14,7 +14,7 @@ export const plugins = [
         },
       },
       releaseNotes: {
-        template: readFileSync(tplFile, 'utf-8'),
+        template: fs.readFileSync(tplFile, 'utf-8'),
       },
     },
   ],
