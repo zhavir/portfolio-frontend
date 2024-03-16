54a08d90da87effea01be46824fc43b34cb3cb57
Andrea Aramini, INI-MBM-BNC (EXT) <andrea.aramini@swisscom.com>
2024-03-16 01:57:11 +0100

    chore: Add configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+
+import colors from 'tailwindcss/colors';
+
+export const content = [
+  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+];
+export const theme = {
+  extend: {
+    backgroundImage: {
+      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      'gradient-conic':
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    },
+    colors: {
+      primary: colors.green,
+      secondary: colors.green,
+      primaryText: colors.white,
+      secondaryText: '#ADB7BE',
+      primaryBorder: '#33353F',
+      primaryBackgrund: 'rgb(18,18,18)',
+      secondaryBackgroud: '#18191E',
+      primaryPlaceholder: '#9CA2A9',
     },
   },
-  plugins: [],
 };
+export const plugins = [];
