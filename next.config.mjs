631fed0c7e77c3b60407afc6b2fae479bb964868
Andrea Aramini, INI-MBM-BNC (EXT) <andrea.aramini@swisscom.com>
2024-03-27 23:41:32 +0100

    :boom: breaking(infrastructure): move from static to ssr

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    loader: 'custom',
-    loaderFile: './imageLoader.js',
-  },
-  env: {
-    IMAGE_URL: process.env.IMAGE_URL,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
