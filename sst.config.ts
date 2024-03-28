a9463f9208a25d098b2a25302f22e7614b5351f6
Andrea Aramini, INI-MBM-BNC (EXT) <andrea.aramini@swisscom.com>
2024-03-28 01:51:46 +0100

    :sparkles: feature: add pipeline for sst

--- sst.config.ts
+++ sst.config.ts
@@ -1,9 +1,6 @@
 import { SSTConfig } from 'sst';
-import { NextjsSite } from 'sst/constructs';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
-import { Duration } from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { StackContext } from 'sst/constructs';
+import { Site } from './stacks/Site';
+import { IAM } from './stacks/IAM';
 
 export default {
   config(_input) {
@@ -13,91 +10,6 @@ export default {
     };
   },
   stacks(app) {
-    app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, 'site', {
-        customDomain: {
-          domainName: 'andreaaramini.space',
-          isExternalDomain: true,
-          cdk: {
-            certificate: Certificate.fromCertificateArn(
-              stack,
-              'MyCert',
-              'arn:aws:acm:us-east-1:992382494893:certificate/28ba9d7c-a946-4fe6-a9f2-07cefca6dcec',
-            ),
-          },
-        },
-      });
-
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
-    });
+    app.stack(Site).stack(IAM);
   },
 } satisfies SSTConfig;
-
-export function IAM({ app, stack }: StackContext) {
-  if (app.stage === 'production') {
-    const provider = new iam.OpenIdConnectProvider(stack, 'GitHub', {
-      url: 'https://token.actions.githubusercontent.com',
-      clientIds: ['sts.amazonaws.com'],
-    });
-
-    // const organization = 'AndreaAramini'; // Use your GitHub organization
-    const repository = 'portfolio-frontend'; // Use your GitHub repository
-
-    new iam.Role(stack, 'GitHubActionsRole', {
-      assumedBy: new iam.OpenIdConnectPrincipal(provider).withConditions({
-        StringLike: {
-          'token.actions.githubusercontent.com:sub': `repo:${repository}:*`,
-        },
-      }),
-      description: 'Role assumed for deploying from GitHub CI using AWS CDK',
-      roleName: 'GitHub', // Change this to match the role name in the GitHub workflow file
-      maxSessionDuration: Duration.hours(1),
-      inlinePolicies: {
-        // You could attach AdministratorAccess here or constrain it even more, but this uses more granular permissions used by SST
-        SSTDeploymentPolicy: new iam.PolicyDocument({
-          assignSids: true,
-          statements: [
-            new iam.PolicyStatement({
-              effect: iam.Effect.ALLOW,
-              actions: [
-                'cloudformation:DeleteStack',
-                'cloudformation:DescribeStackEvents',
-                'cloudformation:DescribeStackResources',
-                'cloudformation:DescribeStacks',
-                'cloudformation:GetTemplate',
-                'cloudformation:ListImports',
-                'ecr:CreateRepository',
-                'iam:PassRole',
-                'iot:Connect',
-                'iot:DescribeEndpoint',
-                'iot:Publish',
-                'iot:Receive',
-                'iot:Subscribe',
-                'lambda:GetFunction',
-                'lambda:GetFunctionConfiguration',
-                'lambda:UpdateFunctionConfiguration',
-                's3:ListBucket',
-                's3:PutObjectAcl',
-                's3:GetObject',
-                's3:PutObject',
-                's3:DeleteObject',
-                's3:ListObjectsV2',
-                's3:CreateBucket',
-                's3:PutBucketPolicy',
-                'ssm:DeleteParameter',
-                'ssm:GetParameter',
-                'ssm:GetParameters',
-                'ssm:GetParametersByPath',
-                'ssm:PutParameter',
-                'sts:AssumeRole',
-              ],
-              resources: ['*'],
-            }),
-          ],
-        }),
-      },
-    });
-  }
-}
