954fd13993fbf7e0d367b09e0b77cee3e0b7b908
Andrea Aramini, INI-MBM-BNC (EXT) <andrea.aramini@swisscom.com>
2024-04-17 01:25:47 +0200

    :bug: fix: fix rerouting

--- src/app/lib/client.js
+++ src/app/lib/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`,
-  timeout: 1000,
+  timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
 
--- stacks/Site.ts
+++ stacks/Site.ts
@@ -1,7 +1,6 @@
 import { NextjsSite } from 'sst/constructs';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 
 export function Site({ stack }) {
   const domainName = 'andreaaramini.space';
@@ -24,27 +23,15 @@ export function Site({ stack }) {
     customDomain: {
       domainName: domainName,
       isExternalDomain: true,
-      alternateNames: [`*.${domainName}`],
       cdk: {
         hostedZone,
         certificate,
       },
     },
+    environment: {
+      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    },
   });
-
-  // Create A and AAAA records for the alternate domain names
-  // Note: CloudFront distribution is not created when running `sst dev`.
-  if (site.cdk?.distribution) {
-    const recordProps = {
-      recordName: `*.${domainName}`,
-      zone: hostedZone,
-      target: route53.RecordTarget.fromAlias(
-        new route53Targets.CloudFrontTarget(site.cdk.distribution),
-      ),
-    };
-    new route53.ARecord(stack, 'AlternateARecord', recordProps);
-    new route53.AaaaRecord(stack, 'AlternateAAAARecord', recordProps);
-  }
 
   stack.addOutputs({
     SiteUrl: site.url,
